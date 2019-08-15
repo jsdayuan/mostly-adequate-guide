322b43505af89526e34e5cfacf50a5df4292296e
dayuan-JS <2191516303@qq.com>
2019-08-15 18:13:03 +0800

    compose练习
    curry 柯里化的函数 要操作的数据放到最后一个参数
    compose 函数无需提及要操作的数据是什么样的
    每个函数都接受一个实际参数

--- src/第五章/index.js
+++ src/第五章/index.js
@@ -21,18 +21,51 @@ console.log('compose')
  * 
  */
 
- //非pointfree
+//非pointfree
 
-var snakeCase=function(word){
-  return word.toLowerCase().replace(' ','_')
+var snakeCase = function (word) {
+  return word.toLowerCase().replace(' ', '_')
 }
 //pointfree
-var snakeCase2=compose(replace(' ','_'),toLowerCase)
+var snakeCase2 = compose(replace(' ', '_'), toLowerCase)
 
 
 //另一个例子
-var initials=function (name){
-  return name.splice(' ').map(compose(toUpperCase,head)).join('')
+var initials = function (name) {
+  return name.splice(' ').map(compose(toUpperCase, head)).join('')
 }
 //pointfree
-var initials2=compose(join('.'),map(compose(toUpperCase,head)),splice(''))
+var initials2 = compose(join('.'), map(compose(toUpperCase, head)), splice(''))
+
+/**
+ * 帮助减少不必要的命名 让代码保持简洁和通用（双刃剑）
+ */
+
+//每个函数都接受一个实际参数
+//debug
+//可以使用这个实用但是不纯的trace函数来追踪代码的执行情况
+
+var trace = curry(function (tag, x) {
+  console.log(tag, x)
+  return x
+})
+
+//范畴学
+
+//对象的搜集
+//把类型当作集合对待 因为我们可以利用集合论处理类型
+
+//态射的搜集
+//态射是标准的、普通的纯函数
+
+//态射的组合
+/**
+ * 这就是组合 
+ * 前面已经提过 compose是符合结合律的 结合律是在范畴学中对任何组合都适用的一种特性
+ * 
+ */
+
+//identity这个独特的态射
+let id = x => x
+//下面这个特性对所有一元函数都成立
+compose(id,f)==compose(f,id)==f
