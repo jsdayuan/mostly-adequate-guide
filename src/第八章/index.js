2be81eb2170c795d231385c4ba7c3ac46ecf985d
dayuan-JS <2191516303@qq.com>
2019-09-04 22:39:20 +0800

    看完第八章 做练习 学习functor 范畴 态射

--- src/第八章/index.js
+++ src/第八章/index.js
@@ -220,12 +220,14 @@ Right.of({ host: 'localhost', port: 80 }).map(prop('host'));
 Left.of("rolls eyes...").map(prop("host"));
 // Left('rolls eyes...')
 
-var either = function (f, g, e) {
-  switch (e.constructor) {
-    case Left: return f(e._value);
-    case Right: return g(e._value)
+var either = _curry(
+  function (f, g, e) {
+    switch (e.constructor) {
+      case Left: return f(e._value);
+      case Right: return g(e._value)
+    }
   }
-}
+)
 
 
 console.log('///////////////////////////////////////////////////////////////')
@@ -272,29 +274,28 @@ console.log(io_window_href._value())
 ////// 纯代码库: lib/params.js ///////
 
 //  url :: IO String
-var url = new IO(function () { return window.location.href; });
+let url = new IO(function () { return window.location.href; });
 
 //  toPairs =  String -> [[String]]
-var toPairs = compose(test('hello22222'), map(split('=')), test('hello'), split('&'));
+let toPairs = compose(map(split('=')), split('&'));
 
 function _last(arr) {
   return arr[arr.length - 1]
 }
 //  params :: String -> [[String]]
-var params = compose(toPairs, _last, split('?'));
+let params = compose(toPairs, _last, split('?'));
 
 let eq = _ => _ => _ //暂不实现
 let head = (arr) => arr[0]
 //  findParam :: String -> IO Maybe [String]
-var findParam = function (key) {
+let findParam = function (key) {
   return map(compose(Maybe.of, filter(compose(eq(key), head)), params), url);
-  //  *********
 };
 
 ////// 非纯调用代码: main.js ///////
 
 // 调用 _value() 来运行它！
-findParam("searchTerm")._value();
+console.log(findParam("searchTerm")._value())
 
 // Maybe(['searchTerm', 'wafflehouse'])
 
@@ -302,6 +303,7 @@ findParam("searchTerm")._value();
  * 把url包裹在IO里 然后把它传递给他的调用者
  * IO(Maybe([String])) 有三层functor
  * （Array）是最有资格成为mappable的容器类型
+ * 
  * map functor 让值在不离开容器的情况下修改值
  * monad 一种值类型
  * 
@@ -351,7 +353,6 @@ console.log(compLaw2(Container.of('')))
  * 代码中的functor实际上是把范畴映射到了一个包含类型和函数的子范畴
  */
 
-
 /**
  * 可以用一张图来表示这种态射及起对象的映射
  * 
@@ -390,3 +391,105 @@ console.log(m2('O'))
  *
  *
  */
+
+//本章已经学习了很多的functor （Maybe IO。。） 
+//用多个functor参数调用同一个函数是怎样的呢  （monad）
+
+// 练习 1
+// ==========
+// 使用 _.add(x,y) 和 _.map(f,x) 创建一个能让 functor 里的值增加的函数
+
+var ex1 = map(add(1))
+
+//练习 2
+// ==========
+// 使用 _.head 获取列表的第一个元素
+
+// var xs = Identity.of(['do', 'ray', 'me', 'fa', 'so', 'la', 'ti', 'do']);
+
+var ex2 = map(head)
+
+
+
+// 练习 3
+// ==========
+// 使用 safeProp 和 _.head 找到 user 的名字的首字母
+var safeProp = _.curry(function (x, o) { return Maybe.of(o[x]); });
+
+var user = { id: 2, name: "Albert" };
+
+var ex3 = compose(map(head), safeProp('name'))(user)
+
+
+// 练习 4
+// ==========
+// 使用 Maybe 重写 ex4，不要有 if 语句
+
+var ex4 = function (n) {
+  if (n) { return parseInt(n); }
+};
+
+var ex4 = compose(map(parseInt), Maybe.of)
+
+
+
+// 练习 5
+// ==========
+// 写一个函数，先 getPost 获取一篇文章，然后 toUpperCase 让这片文章标题变为大写
+
+// getPost :: Int -> Future({id: Int, title: String})
+
+// var getPost = function (i) {
+//   return new Task(function (rej, res) {
+//     setTimeout(function () {
+//       res({ id: i, title: 'Love them futures' })
+//     }, 300)
+//   });
+// }
+
+// let upperTitle = compose(toUpperCase, prop('title'))
+// var ex5 = compose(map(upperTitle), getPost)
+
+
+
+// 练习 6
+// ==========
+// 写一个函数，使用 checkActive() 和 showWelcome() 分别允许访问或返回错误
+
+var showWelcome = compose(add("Welcome "), prop('name'))
+
+var checkActive = function (user) {
+  return user.active ? Right.of(user) : Left.of('Your account is not active')
+}
+
+var ex6 = compose(map(showWelcome), checkActive)
+
+
+
+// 练习 7
+// ==========
+// 写一个验证函数，检查参数是否 length > 3。如果是就返回 Right(x)，否则就返回
+// Left("You need > 3")
+
+var ex7 = function (x) {
+  return x > 3 ? Right.of(x) : Left.of('You need >3') // <--- write me. (don't be pointfree)
+}
+
+
+
+// 练习 8
+// ==========
+// 使用练习 7 的 ex7 和 Either 构造一个 functor，如果一个 user 合法就保存它，否则
+// 返回错误消息。别忘了 either 的两个参数必须返回同一类型的数据。
+
+var save = function (x) {
+  return new IO(function () {
+    console.log("SAVED USER!");
+    return x + '-saved';
+  });
+}
+
+
+let e = either(IO.of, save)
+
+var ex8 = compose(e, ex7)
