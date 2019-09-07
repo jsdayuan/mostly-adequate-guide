034e557b1978f0af696220ce503e8deed2934121
dayuan-JS <2191516303@qq.com>
2019-09-07 23:30:42 +0800

    学习monad join

--- src/第九章/index.js
+++ src/第九章/index.js
@@ -50,11 +50,17 @@ function _test(x, v) {
   return v
 }
 let test = curry(_test)
+
+function _prop(label, obj) {
+  return obj[label]
+}
+let prop = curry(_prop)
+
 //关键点是把任意值丢到容器里 然后到处使用map的能力
 
 // IO Test
 function IO(value) {
-  this._value = value
+  this.unsafePerformIO = value
 }
 
 IO.of = function (x) {
@@ -64,11 +70,11 @@ IO.of = function (x) {
 }
 
 IO.prototype.map = function (f) {
-  return new IO(compose(f, this._value))
+  return new IO(compose(f, this.unsafePerformIO))
 }
 
 let io_test = IO.of('hello ').map(concat('world'))
-console.log(io_test._value())
+console.log(io_test.unsafePerformIO(), 'io_test')
 
 //Maybe Test
 function Maybe(x) {
@@ -87,7 +93,7 @@ Maybe.prototype.map = function (f) {
   return this.isNothing() ? Maybe.of(null) : Maybe.of(f(this._value))
 }
 
-console.log(Maybe.of(123).map(add(1)))
+console.log(Maybe.of(123).map(add(1)), 'maybe_test')
 
 //Either Test
 function Either(x) {
@@ -127,7 +133,7 @@ Right.prototype.map = function (f) {
   return Right.of(f(this._value))
 }
 
-console.log(Either.of('not data').map(concat('123')))
+console.log(Either.of('not data').map(concat('123')), 'either_test')
 
 /**
  * IO的构造器接受一个函数作为参数 Maybe和Either可以接受任意类型
@@ -150,7 +156,6 @@ var readFile = function (filename) {
 //  print :: String -> IO String
 var print = function (x) {
   return new IO(function () {
-    console.log(x);
     return x;
   });
 }
@@ -160,11 +165,21 @@ var print = function (x) {
 //  cat :: IO (IO String)
 var cat = compose(map(print), readFile);
 
-cat(".git/config")._value()._value()
+cat(".git/config").unsafePerformIO().unsafePerformIO()
 // IO(IO("[core]\nrepositoryformatversion = 0\n"))
 
 //包了一层IO 显得怪怪的
 
+
+//另一个例子
+
+//正常的无嵌套
+let normalMaybe = compose(map(prop('street')), map(prop(0)), map(prop('addresses')), Maybe.of)
+console.log(normalMaybe(
+  { addresses: [{ street: { name: 'Mulburry', number: 8402 }, postcode: "WC2N" }] }
+), 'functor_maybe')
+
+//monad
 //  safeProp :: Key -> {Key: a} -> Maybe a
 var safeProp = curry(function (x, obj) {
   return new Maybe(obj[x]);
@@ -180,6 +195,47 @@ var firstAddressStreet = compose(
 
 console.log(firstAddressStreet(
   { addresses: [{ street: { name: 'Mulburry', number: 8402 }, postcode: "WC2N" }] }
-))
+), 'monad')
 // Maybe(Maybe(Maybe({name: 'Mulburry', number: 8402})))
+
+//这里的functor同样也是嵌套的 三个可能失败的地方都用了maybe
+//嵌套的functor是monad的主要使用场景！！
+//monad像洋葱 当我们用map剥开嵌套的functor获取里面的值时 就像剥洋葱一样让人忍不住想哭
+//幸好我们有join
+
+Maybe.prototype.join = function () {
+  return this.isNothing() ? Maybe.of(null) : this._value
+}
+
+IO.prototype.join = function () {
+  return this.unsafePerformIO()
+}
+
+let mmo = Maybe.of(Maybe.of('nun_chucks'))
+console.log(mmo)
+console.log(mmo.join(), 'join_maybe')
+
+let ioio = IO.of(IO.of('hello world'))
+console.log(ioio.unsafePerformIO().unsafePerformIO())
+console.log(ioio.join().unsafePerformIO(), 'join_io')
+
+//如果有两层相同的嵌套 就可以用join把他们压扁到一块去
+//这种结合的能力 functor之间的联姻 就是monad之所以成为monad的原因
+
+/**
+ * monad是可以变扁（flatten）的pointed functor
+ * 一个functor只要他有join和of方法 并遵循一些定律 那么他就是一个monad
+ */
+
+//把monad魔法作用到firstAddressStreet上
+
+//Monad m => m(m a) -> m a
+let join = function (mma) { return mma.join() }
+
+let firstAddressStreet2 = compose(
+  join, map(safeProp('street')), join, map(safeHead), safeProp('addresses')
+)
+console.log(firstAddressStreet2(
+  { addresses: [{ street: { name: 'Mulburry', number: 8402 }, postcode: "WC2N" }] }
+), 'monad_join')
 
